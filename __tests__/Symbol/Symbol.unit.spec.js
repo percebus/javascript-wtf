fe9a4240d71e835a1f8ee4e7521ba7460dd3ab88
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-02-19 15:36:39 -0600

    test( Symbol('that is cool') )
    ```
    # tests 411
    # pass  411
    # skip  0
    # todo  0
    # fail  0
    ```

---
+++ __tests__/Symbol/Symbol.unit.spec.js
@@ -0,0 +1,59 @@
+describe('Symbol', () => {
+  'use strict'
+  describe('string', () => {
+    let oSymbol
+
+    describe("Symbol('that is cool')", () => { // https://www.youtube.com/watch?v=2pL28CcEijU
+      beforeEach(() => {
+        oSymbol = Symbol('that is cool')
+      })
+
+      describe('convert', () => { // it can be converted to String
+        const expectedString = 'Symbol(that is cool)'
+        describe('String(oSymbol)', () => {
+          it("can be converted Symbol('that is cool')", () => {
+            expect(String(oSymbol)).toEqual(expectedString)
+            expect(oSymbol.toString()).toEqual(expectedString)
+          })
+        })
+
+        describe('oSymbol.toString()', () => {
+          it("can be converted Symbol('that is cool')", () => {
+            expect(String(oSymbol)).toEqual(expectedString)
+            expect(oSymbol.toString()).toEqual(expectedString)
+          })
+        })
+      })
+
+      describe('cast', () => { // it cannot be casted to String
+        describe("Symbol + ''", () => {
+          function catchError () {
+            return function concatSymbol () {
+              return oSymbol + ''
+            }
+          }
+
+          it('throws a TypeError', () => {
+            expect(catchError()).toThrow()
+          })
+
+          xdescribe('error message', () => {
+            describe('common', () => {
+              it("throws a TypeError: 'Cannot convert a Symbol value to a string'", () => {
+                const oTypeError = new TypeError('Cannot convert a Symbol value to a string')
+                expect(catchError()).toThrow(oTypeError)
+              })
+            })
+
+            describe('Safari', () => {
+              it("throws a TypeError: 'Cannot convert a symbol to a string'", () => {
+                const oTypeError = new TypeError('Cannot convert a symbol to a string')
+                expect(catchError()).toThrow(oTypeError)
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+})
