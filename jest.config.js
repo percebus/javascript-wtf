9c1c2ba0b9a713daf53da275cf7d6f48fdfec942
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-02-20 22:33:42 -0600

    fix test

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = () => {
     verbose: true,
     globals: {
       _: require('lodash') // we inject here, so we can use testem to test browsers
-    }
+    },
+    transformIgnorePatterns: [
+      '<rootDir>/node_modules/',
+      '<rootDir>/node_modules/util-deprecate'
+    ]
   }
 }
