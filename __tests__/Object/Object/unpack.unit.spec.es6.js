9466ae80cfc26eee28c18356e637d4e183dd3012
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-02-20 18:54:00 -0600

    test( newCoordinate3(({latitude:180, longitude:90}) )
    ```
    # tests 633
    # pass  633
    # skip  0
    # todo  0
    # fail  0
    ```

--- __tests__/Object/Object/unpack.unit.spec.es6.js
+++ __tests__/Object/Object/unpack.unit.spec.es6.js
@@ -2,15 +2,26 @@ describe('Object', () => {
   'use strict'
 
   describe('newCoordinate(longitude, latitude)', () => { // SRC: https://www.youtube.com/watch?v=2pL28CcEijU
-    const factories = {
-      '{longitude:x, latitude:y}': function newCoordinate (x, y) { return { longitude: x, latitude: y } },
-      '{ longitude, latitude }': function newCoordinate (longitude, latitude) { return { longitude, latitude } }
+    function newCoordinate1 (x, y) {
+      return { longitude: x, latitude: y }
     }
 
-    _.forEach(factories, (newCoordinate, testName) => {
-      describe(testName, () => {
-        const coordinate = newCoordinate(180, 90)
+    function newCoordinate2 (longitude, latitude) {
+      return { longitude, latitude }
+    }
 
+    function newCoordinate3 ({ longitude, latitude }) {
+      return { longitude, latitude }
+    }
+
+    const coordinates = {
+      '{longitude:x, latitude:y}': newCoordinate1(180, 90),
+      '{ longitude, latitude }': newCoordinate2(180, 90),
+      '({latitude:180, longitude:90})': newCoordinate3({ latitude: 90, longitude: 180 })
+    }
+
+    _.forEach(coordinates, (coordinate, testName) => {
+      describe(testName, () => {
         describe('unpacking', () => {
           describe('longitude, latitude = coordinate', () => {
             const { longitude, latitude } = coordinate
@@ -31,7 +42,8 @@ describe('Object', () => {
           })
 
           describe('{longitude:x, latitude:y}', () => {
-            const { longitude: x, latitude: y } = coordinate
+            // const { x: longitude, y: latitude } = coordinate // I would expect THIS
+            const { longitude: x, latitude: y } = coordinate // not THIS
 
             describe('x', () => {
               it('equals 180, from coordinate.longitude', () => {
