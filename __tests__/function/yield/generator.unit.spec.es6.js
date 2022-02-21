d4480e30ffc5b4c727662048e5fa61b19d047cda
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-02-20 22:04:57 -0600

    test(iterator)
    ```
    # tests 672
    # pass  672
    # skip  0
    # todo  0
    # fail  0
    ```

--- __tests__/function/yield/generator.unit.spec.es6.js
+++ __tests__/function/yield/generator.unit.spec.es6.js
@@ -1,12 +1,8 @@
-/* FIXME! including this file,
- * UNINSTALLS (affects?!) node_modules/lodash!
- * Breaking a bunch of OTHER 'unit' tests
- * Hence, the '*.js.FIXME' extension
- */
 describe('function', () => {
   'use strict'
+
   describe('iterators', () => {
-    describe('1, 2, 3', () => {
+    describe('*createIterator(i)', () => {
       function * createIterator (i) {
         while (i++ <= 2) {
           yield i
@@ -14,11 +10,25 @@ describe('function', () => {
       }
 
       describe('iterator.next()', () => {
-        it('returns 1, 2 & 3', () => {
+        it('yields 1, 2 & 3', () => {
           const iterator = createIterator(0)
           expect(iterator.next()).toEqual({ value: 1, done: false })
           expect(iterator.next()).toEqual({ value: 2, done: false })
           expect(iterator.next()).toEqual({ value: 3, done: false })
+          expect(iterator.next()).toEqual({ value: undefined, done: true })
+          expect(iterator.next()).toEqual({ value: undefined, done: true })
+        })
+      })
+
+      describe('iterator.return(9000)', () => {
+        it('skips to the end', () => {
+          const iterator = createIterator(0)
+          expect(iterator.next()).toEqual({ value: 1, done: false })
+          expect(iterator.return(10)).toEqual({ value: 10, done: true })
+          expect(iterator.next()).toEqual({ value: undefined, done: true })
+          expect(iterator.next()).toEqual({ value: undefined, done: true })
+          expect(iterator.return(9000)).toEqual({ value: 9000, done: true })
+          expect(iterator.next()).toEqual({ value: undefined, done: true })
           expect(iterator.next()).toEqual({ value: undefined, done: true })
         })
       })
