40ea2cf8f27e693535518dc3fa5a620b10cbe35c
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-02-20 18:46:21 -0600

    test(unpacking)
    ```
    # tests 621
    # pass  621
    # skip  0
    # todo  0
    # fail  0
    ```

--- __tests__/Object/Object/unpack.unit.spec.es6.js
+++ __tests__/Object/Object/unpack.unit.spec.es6.js
@@ -1,27 +1,52 @@
 describe('Object', () => {
   'use strict'
 
-  describe('unpacking', () => { // SRC: https://www.youtube.com/watch?v=2pL28CcEijU
-    function newCoordinate () {
-      return { longitude: 180, latitude: 90 }
+  describe('newCoordinate(longitude, latitude)', () => { // SRC: https://www.youtube.com/watch?v=2pL28CcEijU
+    const factories = {
+      '{longitude:x, latitude:y}': function newCoordinate (x, y) { return { longitude: x, latitude: y } },
+      '{ longitude, latitude }': function newCoordinate (longitude, latitude) { return { longitude, latitude } }
     }
 
-    const coordinate = newCoordinate()
+    _.forEach(factories, (newCoordinate, testName) => {
+      describe(testName, () => {
+        const coordinate = newCoordinate(180, 90)
 
-    describe('{ longitude, latitude } = newCoordinate()', () => {
-      const { longitude, latitude } = coordinate
+        describe('unpacking', () => {
+          describe('longitude, latitude = coordinate', () => {
+            const { longitude, latitude } = coordinate
 
-      describe('longitude', () => {
-        it('equals 180, from coordinate.longitude', () => {
-          expect(longitude).toBe(coordinate.longitude)
-          expect(longitude).toBe(180)
-        })
-      })
+            describe('longitude', () => {
+              it('equals 180, from coordinate.longitude', () => {
+                expect(longitude).toBe(coordinate.longitude)
+                expect(longitude).toBe(180)
+              })
+            })
 
-      describe('latitude', () => {
-        it('equals 90, from coordinate.latitude', () => {
-          expect(latitude).toBe(coordinate.latitude)
-          expect(latitude).toBe(90)
+            describe('latitude', () => {
+              it('equals 90, from coordinate.latitude', () => {
+                expect(latitude).toBe(coordinate.latitude)
+                expect(latitude).toBe(90)
+              })
+            })
+          })
+
+          describe('{longitude:x, latitude:y}', () => {
+            const { longitude: x, latitude: y } = coordinate
+
+            describe('x', () => {
+              it('equals 180, from coordinate.longitude', () => {
+                expect(x).toBe(coordinate.longitude)
+                expect(x).toBe(180)
+              })
+            })
+
+            describe('y', () => {
+              it('equals 90, from coordinate.latitude', () => {
+                expect(y).toBe(coordinate.latitude)
+                expect(y).toBe(90)
+              })
+            })
+          })
         })
       })
     })
