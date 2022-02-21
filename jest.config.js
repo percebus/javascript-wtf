191860a6e7ba0287caa8ec427a4cd94094bbf71c
J. C. Guerrero <Juan.Guerrero@HERE.com>
2022-02-21 08:52:44 -0600

    fix(es6.js)
    ```
    # tests 666
    # pass  666
    # skip  0
    # todo  0
    # fail  0
    ```

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = () => {
     globals: {
       _: require('lodash') // we inject here, so we can use testem to test browsers
     },
+    transform: {
+      '^.+\\.es6\\.js?$': 'babel-jest'
+    },
     transformIgnorePatterns: [
       '<rootDir>/node_modules/',
       '<rootDir>/node_modules/util-deprecate'
