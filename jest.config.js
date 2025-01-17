32f0de1974a97dc633fe8ad4029ffcd8f9554bf1
JCGuerrero <JCystems@gMail.com>
2025-01-16 22:30:06 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,21 @@
 module.exports = () => {
   return {
     verbose: true,
+    setupFiles: ['<rootDir>/__tests__/global.js'],
     globals: {
       _: require('lodash') // we inject here, so we can use testem to test browsers
     },
-    setupFiles: ['<rootDir>/__tests__/global.js'],
     transform: {
       '^.+\\.es6\\.js$': 'babel-jest'
     },
     transformIgnorePatterns: [
       '<rootDir>/node_modules/',
       '<rootDir>/node_modules/util-deprecate'
+    ],
+    reporters: [
+      'default',
+      // 'github-actions', // FIXME
+      'jest-junit'
     ]
   }
 }
